28c27ee39e69e562525c1405cc53b6b3a0c2337f
Adrián Ciborro Montes <37084114+adcimon@users.noreply.github.com>
2022-03-29 12:27:47 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,15 +2,37 @@ import torch
 
 def print_backend_support():
 
-    print('Processing unit: {}'.format('GPU' if torch.cuda.is_available() else 'CPU'))
+    cuda_built = torch.backends.cuda.is_built()
+    cuda_available = torch.cuda.is_available()
 
-    if torch.cuda.is_available():
+    cudnn_version = torch.backends.cudnn.version()
+    cudnn_available = torch.backends.cudnn.is_available()
+    cudnn_enabled = torch.backends.cudnn.enabled
 
+    mkl_available = torch.backends.mkl.is_available()
+    mkldnn_available = torch.backends.mkldnn.is_available()
+    openmp_available = torch.backends.openmp.is_available()
+
+    print('CUDA: {} | {}'.format(
+        'Built' if cuda_built else 'Not Built',
+        'Available' if cuda_available else 'Not Available'))
+
+    print('cuDNN: {} | {} ({})'.format(
+        'Available' if cudnn_available else 'Not Available',
+        'Enabled' if cudnn_enabled else 'Not Enabled',
+        cudnn_version))
+
+    print('MKL: {}'.format('Available' if mkl_available else 'Not Available'))
+    print('MKL-DNN: {}'.format('Available' if mkldnn_available else 'Not Available'))
+    print('OpenMP: {}'.format('Available' if openmp_available else 'Not Available'))
+
+    print('Processing unit: {}'.format('GPU' if cuda_available else 'CPU'))
+    if cuda_available:
         for i in range(torch.cuda.device_count()):
             if i == torch.cuda.current_device():
-                print('\t[x] {}'.format(torch.cuda.get_device_name(i)))
+                print('[x] {}'.format(torch.cuda.get_device_name(i)))
             else:
-                print('\t[ ] {}'.format(torch.cuda.get_device_name(i)))
+                print('[ ] {}'.format(torch.cuda.get_device_name(i)))
 
 def print_device_usage(device):
 
